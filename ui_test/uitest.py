ae0598a5781073c4aa32886225057c683f61bdac
panyingji <759695808@qq.com>
2019-07-24 10:32:50 +0800

    0724

--- ui_test/uitest.py
+++ ui_test/uitest.py
@@ -14,10 +14,13 @@ import time
 
 driver = webdriver.Chrome()
 
+# 设置隐式等待（周期性检查元素是否出现，每隔半秒检查一次）
+driver.implicitly_wait(10)
+
 driver.get('https://www.baidu.com')
 driver.find_element_by_id('kw').send_keys('松勤')
 driver.find_element_by_id('su').click()
-time.sleep(1)
+# time.sleep(1)
 res = driver.find_element_by_id('1')
 res2 = res.text
 
